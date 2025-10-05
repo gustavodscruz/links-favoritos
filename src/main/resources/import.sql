99a54899732f5dddc64f031060e011d799c1d060
gustavodscruz <gustavodiasdsc@gmail.com>
2025-10-05 00:48:35 -0300

    delete import

--- src/main/resources/import.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO dbo.categoria (name, user_id) VALUES ('Trabalho', 1);
-INSERT INTO dbo.categoria (name, user_id) VALUES ('Pessoal', 1);
-INSERT INTO dbo.categoria (name, user_id) VALUES ('Estudos', 1);
-INSERT INTO dbo.categoria (name, user_id) VALUES ('Lazer', 1);
-INSERT INTO dbo.categoria (name, user_id) VALUES ('Favoritos', 1);
-INSERT INTO dbo.categoria (name, user_id) VALUES ('Importantes', 1);
